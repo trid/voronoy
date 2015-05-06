1eb83f9faf2fa7dad47ff01ab31f017ccb8cac06
TriD <hovyakov@gmail.com>
2015-05-07 01:33:21 +0300

    Started edges building implementation

--- main.cpp
+++ main.cpp
@@ -20,7 +20,12 @@ public:
     vector<Point*> connections;
 };
 
-const int pointsCount = 128;
+class Edge {
+public:
+    int x1, y1, x2, y2;
+};
+
+const int pointsCount = 10;
 vector <Point> points(pointsCount);
 SDL_Surface* surface;
 SDL_Window* window;
@@ -30,6 +35,20 @@ void putPoints();
 void generateBackground();
 
 int getClosestPoint(int x, int y);
+
+bool compare(const Point &, const Point &);
+
+class Event{
+    virtual void process() = 0;
+};
+
+class SiteEvent: public Event {
+
+};
+
+class CircleEvent: public Event {
+
+};
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -60,16 +79,23 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+//void generateBackground() {
+//    surface = SDL_CreateRGBSurface(SDL_GetWindowPixelFormat(window), 800, 600, 32, 0, 0, 0, 0);
+//
+//    for (int x = 0; x < 800; x ++) {
+//        for (int y = 0; y < 600; y++) {
+//            SDL_Color point = points[getClosestPoint(x, y)].color;
+//            ((Uint32*)surface->pixels)[y * 800 + x] = SDL_MapRGB(surface->format, point.r, point.g, point.b);
+//        }
+//    }
+//}
+
 void generateBackground() {
     surface = SDL_CreateRGBSurface(SDL_GetWindowPixelFormat(window), 800, 600, 32, 0, 0, 0, 0);
 
-    for (int x = 0; x < 800; x ++) {
-        for (int y = 0; y < 600; y++) {
-            SDL_Color point = points[getClosestPoint(x, y)].color;
-            ((Uint32*)surface->pixels)[y * 800 + x] = SDL_MapRGB(surface->format, point.r, point.g, point.b);
-        }
-    }
+
 }
+
 
 int getClosestPoint(int x, int y) {
     int closestDist = (points[0].x - x) * (points[0].x - x) + (points[0].y - y) * (points[0].y - y);
@@ -95,5 +121,9 @@ void putPoints() {
         points[i].color.b = (Uint8) (rand() / 256);
         points[i].color.a = (Uint8) 255;
     }
-    sort(points.begin(), points.end(), [](const Point& p1, const Point& p2){return p1.x < p2.x;});
+    sort(points.begin(), points.end(), compare);
+}
+
+bool compare(const Point &p1, const Point &p2) {
+    return p1.y < p2.y;
 }
